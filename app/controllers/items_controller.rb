f2cc6072e0581b8ad80a785750c346e01014f026
37taichi <68590842+37taichi@users.noreply.github.com>
2020-08-30 17:56:12 +0900

    商品編集の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -21,8 +21,7 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
-    item.update(item_params)
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to action: :show
     else
       render :edit
